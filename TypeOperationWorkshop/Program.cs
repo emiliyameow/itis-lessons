09e29d090fcbf4c7b9239c6b2fc1abd95a8565f7
Emilia Zagitova <emiliyameow@mail.ru>
2024-11-21 09:22:36 +0300

    Add changes

--- TypeOperationWorkshop/Program.cs
+++ TypeOperationWorkshop/Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        string name = Console.ReadLine();
+        /*string name = Console.ReadLine();
         int age = int.Parse(Console.ReadLine());
         int course = int.Parse(Console.ReadLine());
         string group = Console.ReadLine();
@@ -14,6 +14,52 @@ class Program
         Console.WriteLine("Возраст: " + age + " лет");
         Console.WriteLine("Курс: " + course);
         Console.WriteLine("Группа: " + group);
-        Console.WriteLine("Домашний питомец: " + hasHomePet);
+        Console.WriteLine("Домашний питомец: " + hasHomePet);*/
+        try
+        {
+          Console.WriteLine(CalculateOp(10, 5, '-'));
+          Console.WriteLine(CalculateOp(10, 5, '+'));
+          Console.WriteLine(CalculateOp(10, 5, '*'));
+          Console.WriteLine(CalculateOp(10, 5, '^'));
+          Console.WriteLine(CalculateOp(13, 3, '/'));
+          Console.WriteLine(CalculateOp(10, 5, '&'));
+        }
+        catch (DivideByZeroException ex)
+        {
+          Console.WriteLine("Произошла ошибка - нельзя делить на 0!");
+        }
+        catch (Exception ex)
+        {
+          Console.WriteLine("Произошла ошибка: " + ex.Message);
+        }
+
+    }
+
+    static double CalculateOp(long a, long b, char operand)
+    {
+        return operand == '+'
+          ? a + b
+          : operand == '-'
+            ? a - b
+            : operand == '*'
+              ? a * b
+              : operand == '/'
+                ? (double) a / b
+                :
+        switch (operand)
+        {
+            case '+':
+              return a + b;
+            case '-':
+              return a - b;
+            case '/':
+              return ((double)a / b);
+            case '*':
+              return (a * b);
+            case '^':
+              return Math.Pow(a, b);
+            default:
+              throw new NotSupportedException("operand is not supported!");
+        }
     }
 }
